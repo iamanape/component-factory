037907f3f314d32654919e2a7ee0a2b9f2091515
iamanape <amianape@gmail.com>
2025-10-01 13:57:19 -0500

    feat: add component factory implementation

---
+++ types/react/jsx-runtime.d.ts
@@ -0,0 +1,4 @@
+export const Fragment: any;
+export function jsx(type: any, props: any, key?: any): any;
+export function jsxs(type: any, props: any, key?: any): any;
+export function jsxDEV(type: any, props: any, key?: any): any;
